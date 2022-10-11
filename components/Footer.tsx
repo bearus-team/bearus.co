4eb11c70e0c43b4c4fd66e2eadc0c8b007d7bcd9
송영민 <jakgon@naver.com>
2022-10-11 22:10:15 +0900

    feat: 사업자 번호 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,15 @@ function Page() {
       style={{
         width: "100%",
         height: "150px",
-        backgroundColor: "#f1ffff",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         borderTop: "1px solid rgba(0, 27, 55, 0.1)",
       }}
     >
-      ⓒ 2022 bearus. All rights reserved
+      <div>베어어스 | 사업자 등록번호: 463-75-00397 | 대표자: 성형주</div>
+      <div>ⓒ 2022 bearus. All rights reserved</div>
     </div>
   );
 }
